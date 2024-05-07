81e9ee690f98e0f2ecdb8893961ed058f0571ed3
Victoriyashalini <131896241+Victoriyashalini@users.noreply.github.com>
2024-05-07 21:38:46 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { CompanyComponent } from './views/company/company.component';
+import { DestinationComponent } from './views/destination/destination.component';
+import { PackagesComponent } from './views/packages/packages.component';
+import { PopulartoursComponent } from './views/populartours/populartours.component';
+import { ContactComponent } from './views/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{path:'home',title:'home',component:HomeComponent},
+	{path:'company',title:'about',component:CompanyComponent},
+	{path:'destination',title:'destination',component:DestinationComponent},
+	{path:'package',title:'package',component:PackagesComponent},
+	{path:'populartour',title:'populartour',component:PopulartoursComponent},
+	{path:'contact',title:'contact',component:ContactComponent},
+
+
+
+
+];
